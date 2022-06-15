084fdf2164d72cb7662aff6c77ec99065a91c029
MarcuMonica <monica_me5@yahoo.com>
2022-06-15 20:55:52 +0300

    initial commit

---
+++ pages/index.jsx
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Thread Affair</title>
+      </Head>
+
+<header className="header">Hello
+<img src="/images/logo.jpg" alt="" ></img>
+<i className="fa-solid fa-phone"></i>
+</header>
+
+<main className="content"></main>
+
+<footer className="footer"></footer>
+</div>
+  );
+}
