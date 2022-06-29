35a109c01d5f445bc01939293b484c9b85d28709
MarcuMonica <monica_me5@yahoo.com>
2022-06-29 22:01:13 +0300

    Proiect Thread Affair -almost there

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,6 +5,7 @@ import HomePayment from '../components/homepage/HomePayment';
 import HomeSocial from '../components/homepage/HomeSocial';
 import HomeNewsletter from '../components/homepage/HomeNewsletter';
 import ContentBanner from '../components/homepage/ContentBanner';
+import CatalogPreviews from '../components/homepage/CatalogPreviews';
 
 export default function Home() {
   return (
@@ -16,11 +17,13 @@ export default function Home() {
       <Header></Header>
 
       <main className="content">
-        <header className="content-header mb-5">
+        <header className="content-header mb-5g">
           <ContentBanner></ContentBanner>
         </header>
 
         <section className="content-main">
+          <CatalogPreviews></CatalogPreviews>
+
           <HomeNewsletter></HomeNewsletter>
 
           <HomeSocial></HomeSocial>
